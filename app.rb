c9a559d02a951fd3290ebd27f388bddc0eba1982
Mark Sun <msun908@berkeley.edu>
2017-09-05 03:19:14 +0000

    almost done

--- app.rb
+++ app.rb
@@ -57,7 +57,14 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    game_status = @game.check_win_or_lose
+    if game_status == :win
+      redirect '/win'
+    elsif game_status == :lose
+      redirect '/lose'
+    else
+      erb :show # You may change/remove this line
+    end
   end
   
   get '/win' do
